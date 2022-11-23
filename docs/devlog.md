34be3a0ea313bede68f6b221273358661140858e
Danny Ayers <danny.ayers@gmail.com>
2022-11-23 20:30:42 +0100

    getting there

--- docs/devlog.md
+++ docs/devlog.md
@@ -5,3 +5,15 @@ oops
 ng generate component hello
 
 ng build
+
+ng serve
+
+put on github
+
+https://vercel-poke-jjdzl4ecs-danja.vercel.app/hello
+
+ng generate service config
+
+lots from https://angular.io/guide/http
+
+hmm, seems unnecessary, aims to run a server locally
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hello',
@@ -6,13 +11,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './hello.component.html',
   styleUrls: ['./hello.component.css']
 })
+
 export class HelloComponent implements OnInit {
 
-  value = 'World';
+  stuff: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.http.get("https://hyperdata.it", { responseType: 'text', observe: 'response' },).subscribe({
+      next: data => {
+        this.stuff = JSON.stringify(data);
+
+      },
+      error: error => {
+        this.stuff = error.message;
+        console.error('There was an error!', error);
+      },
+
+    });
+  }
+
+  getStuff() {
+    let stuff = this.http.get<HelloComponent>("https://hyperdata.it");
+  }
 }
+
+
+
+
